ec18b2ef536ad92d756a0552b36c5bca32926dd5
Unknown <leo.faucher@edu.itescia.fr>
2019-05-27 16:41:16 +0200

    Init base
    Importation des fichiers de base avec les premières modifications

---
+++ src/main/java/fr/rphstudio/chess/game/ChessModel.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rphstudio.chess.game;
+
+import fr.rphstudio.chess.interf.EmptyCellException;
+import fr.rphstudio.chess.interf.IChess;
+import fr.rphstudio.chess.interf.OutOfBoardException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author fauchetleo
+ */
+public class ChessModel implements IChess {
+    
+    private static ChessModel instance; 
+    public ArrayList board;
+
+    public static IChess getInstance() {
+        if(instance == null){
+            instance = new ChessModel();
+        }
+        return instance;
+    }
+
+    @Override
+    public void reinit() {
+    }
+
+    @Override
+    public ChessType getPieceType(ChessPosition p) throws EmptyCellException, OutOfBoardException {
+        throw new EmptyCellException();
+    }
+
+    @Override
+    public ChessColor getPieceColor(ChessPosition p) throws EmptyCellException, OutOfBoardException {
+        throw new EmptyCellException();
+    }
+
+    @Override
+    public int getNbRemainingPieces(ChessColor color) {
+        return 0;
+    }
+
+    @Override
+    public List<ChessPosition> getPieceMoves(ChessPosition p) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void movePiece(ChessPosition p0, ChessPosition p1) {
+    }
+
+    @Override
+    public ChessKingState getKingState(ChessColor color) {
+        return ChessKingState.KING_SAFE;
+    }
+
+    @Override
+    public List<ChessType> getRemovedPieces(ChessColor color) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public boolean undoLastMove() {
+        return false;
+    }
+
+    @Override
+    public long getPlayerDuration(ChessColor color, boolean isPlaying) {
+        return 0;
+    }
+    
+}
---
+++ src/main/java/fr/rphstudio/chess/game/Piece.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rphstudio.chess.game;
+
+import fr.rphstudio.chess.interf.IChess.ChessColor;
+import fr.rphstudio.chess.interf.IChess.ChessType;
+
+/**
+ *
+ * @author fauchetleo
+ */
+public class Piece {
+    private ChessColor color;
+    private ChessType type;
+}
